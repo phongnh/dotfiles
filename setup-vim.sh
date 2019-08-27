858359823265c419fc0fefb2218be37119a635e6
Phong Nguyen <nhphong1406@gmail.com>
2019-08-27 17:38:23 +0700

    Add setup-vim.sh script

---
+++ setup-vim.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.vim/{plugged,snippets,cache,autoload}
+mkdir -p ~/.vim/snippets/{UltiSnips,snippets,neosnippets}
+mkdir -p ~/.cache/{swap,backup,undo}
+touch ~/.cache/NERDTreeBookmarks
+
+# nvim
+mkdir -p ~/.config
+if [ -L ~/.config/nvim ]; then
+    rm ~/.config/nvim
+    ln -sf ~/.vim ~/.config/nvim
+fi
+
+# Install vim-plug
+if [ ! -s ~/.vim/autoload/plug.vim ]; then
+    curl --silent -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+# Copy vim configs and related ignores
+cp agignore ~/.agignore
+cp ignore ~/.ignore
+cp ctags ~/.ctags
+if [ -d ~/.ctags.d ]; then
+    cp -r ctags.d/ ~/.ctags.d/
+else
+    cp -r ctags.d ~/.ctags.d
+fi
+cp gtags.conf ~/.gtags.conf
+cp vimrc ~/.vimrc
+cp gvimrc ~/.gvimrc
+cp vimrc.before ~/.vimrc.before
+cp init.vim.before ~/.init.vim.before
